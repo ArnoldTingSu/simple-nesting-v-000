5fbe0afe0cdc03e2c59c8b9a57683e9dbb447404
Arnold Su <arnoldtingsu@gmail.com>
2018-11-18 08:36:28 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -60,7 +60,9 @@ def dennis_ritchies_language
         }
      }
      
-     puts #{programmer_hash[:dennis_ritchie][:languages]}
+      {programmer_hash[:dennis_ritchie][:languages] = string
+      
+      puts string
      
 end
 
